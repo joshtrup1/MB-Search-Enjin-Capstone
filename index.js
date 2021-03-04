b51b7ff6109f7ecf1635b04e68641ac31817c3b4
Joshua Trup <50431665+joshtrup1@users.noreply.github.com>
2021-03-04 13:45:35 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+
+app.set("view engine","ejs");
+app.use(express.static("public"));
+
+
+
+//root route
+app.get('/', async function(req, res) {
+ 
+  res.render('home' );
+});
+
+//page 1 route
+app.get('/page1', async function(req, res) {
+
+  res.render('page1' );
+});
+
+//page 2 route
+app.get('/page2', async function(req, res) {
+
+  res.render('page2' );
+});
+
+
+app.listen(3000, () => {
+  console.log('server started');
+});
+
+
+//Google API key: AIzaSyAhu_Juf2lfJuo6EdYxozRB_aQd9ojWzsY
