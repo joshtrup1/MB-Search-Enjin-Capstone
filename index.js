32cf1a1f57273ce1ccc133ad046b9a8fbd000dbc
josh <joshtrup0@gmail.com>
2021-05-07 11:16:16 -0700

    changed port

--- index.js
+++ index.js
@@ -63,7 +63,8 @@ function classType(className) {
 };
 
 
-app.listen(8000, () => {
-  console.log('server started');
+const PORT = process.env.PORT || 3000;   
+app.listen(PORT, () => {    
+  console.log(`Server is running on port ${PORT}.`);   
 });
 
